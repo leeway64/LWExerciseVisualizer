6a490911f003e23ee250dab4935156d266e778f9
Lee-Way Wang <lee.way56@comcast.net>
2021-06-30 19:45:49 -0700

    Initiall commit for Exercise_Visualizer.py
    - Also update .gitignore

---
+++ Exercise_Visualizer.py
@@ -0,0 +1,24 @@
+# This program visualizes several exercise statistics
+
+import pandas as pd
+
+# Create graph of times exercised vs. the month
+def frequency_vs_months_chart():
+    pass
+
+# Create bar chart of times exercised in each day of the week
+def frequency_vs_day_chart():
+    pass
+
+# Create bar chart of how many times I've done each type of exercise
+def frequency_vs_exercise_type():
+    pass
+
+def main():
+    frequency_vs_months_chart()
+    frequency_vs_day_chart()
+    frequency_vs_exercise_type()
+
+if __name__ == '__main__':
+    main()
+
