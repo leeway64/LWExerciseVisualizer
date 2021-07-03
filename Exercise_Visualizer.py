ee6344d3dac30bdaf423d2b8c10788158a1ae46c
Lee-Way Wang <lee.way56@comcast.net>
2021-07-03 14:15:23 -0700

    Start work on sum_column function
    Also:
    - Update frequency_vs_months_chart and frequency_vs_exercise_type functions
    - Start to write tests for sum_column

--- Exercise_Visualizer.py
+++ Exercise_Visualizer.py
@@ -18,7 +18,16 @@ def cell_to_list(cell):
     cell_list = [int(element) for element in cell_list]  # Convert each element into an int
     return cell_list
 
-# Finds the sum of all the length of all the lists in a row
+
+# Finds the sum of the lengths of all the lists in a column
+def sum_column(column):
+    sum = 0
+    for cell in column:
+        sum = len(cell_to_list(cell)) + sum
+    return sum
+
+
+# Finds the sum of the lengths of all the lists in a row
 def sum_row(row):
     sum = 0
     row = row[2:]  # First 2 rows will always be year and month
@@ -26,32 +35,40 @@ def sum_row(row):
         sum = len(cell_to_list(cell)) + sum
     return sum
 
+
 # Create graph of times exercised vs. the month
 def frequency_vs_months_chart(DF):
     days_exercised_in_month = {}
-    for cell in DF.iloc:
-        pass
-    pass
+    for row in DF.iloc:
+        days_exercised = sum_row(row)
+
 
 # Create bar chart of times exercised in each day of the week
 def frequency_vs_day_chart(DF):
     pass
 
+
 # Create bar chart of how many times I've done each type of exercise
 def frequency_vs_exercise_type(DF):
-    pass
+    exercise_list = list(DF.columns)[2:]  # Ignore the year and month columns
+    frequency_of_exercise = dict()
+    for exercise in exercise_list:
+        frequency_of_exercise[exercise] = sum_column(DF[exercise])
+
 
 # Highlight the days of the year where I exercised
 def print_exercise_days(DF):
     pass
 
+
 def main():
     exercise_data = pd.read_excel(r'C:\Users\leewa\Documents\Important documents\Computer Science\Python Projects\Exercise_Tracking_and_Visualization\Exercise tracking.xlsx')
     exercise_data.drop(exercise_data.columns[exercise_data.columns.str.contains('unnamed', case = False)], axis = 1, inplace = True)
+    exercise_data.reset_index(drop = True, inplace=True)
     frequency_vs_months_chart(exercise_data)
     frequency_vs_day_chart(exercise_data)
     frequency_vs_exercise_type(exercise_data)
 
+
 if __name__ == '__main__':
     main()
-
--- test_harness.py
+++ test_harness.py
@@ -2,6 +2,10 @@ import pandas as pd
 from Exercise_Visualizer import cell_to_list, sum_row
 
 # Test harness
+df = pd.read_excel(r'C:\Users\leewa\Documents\Important documents\Computer Science\Python Projects\Exercise_Tracking_and_Visualization\Exercise tracking (testing version).xlsx')
+df.drop(df.columns[df.columns.str.contains('unnamed',case = False)],axis = 1, inplace = True)
+df.reset_index(drop = True, inplace=True)
+
 
 # Testing cell_to_list function
 assert cell_to_list("[]") == []
@@ -12,7 +16,10 @@ assert cell_to_list("[10,2,38,900,1000000]") == [10,2,38,900,1000000]
 
 
 # Testing sum_row function
-df = pd.read_excel(r'C:\Users\leewa\Documents\Important documents\Computer Science\Python Projects\Exercise_Tracking_and_Visualization\Exercise tracking (testing version).xlsx')
-df.drop(df.columns[df.columns.str.contains('unnamed',case = False)],axis = 1, inplace = True)
 assert sum_row(df.iloc[0]) == 8
 assert sum_row(df.iloc[1]) == 19
+assert sum_row(df.iloc[2]) == 6
+
+
+# Testing sum_column function
+assert sum_col() == 23
